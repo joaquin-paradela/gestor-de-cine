1bcb41c7a0dea83e046dfc3e75eb3781a421cde2
Joaquin Paradela <joaquin.paradela28@gmail.com>
2023-05-26 01:37:56 -0300

    footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,43 @@
+<footer class="row align-items-center">
+            <div class="col-lg-4">
+                <ul class="nav justify-content-center">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#"><img src="{{ asset('Imagenes/ICONOS/facebook.png') }}" alt="facebook" id="facebook"></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#"><img src="{{ asset('Imagenes/ICONOS/instagram.png') }}" alt="instagram" id="instagram"></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#"><img src="{{ asset('Imagenes/ICONOS/twitter.png') }}" alt="twitter" id="twitter"></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#"><img src="{{ asset('Imagenes/ICONOS/linkedin.png') }}" alt="linkedin"></a>
+                  </li>
+                </ul>
+            </div>
+
+            <div class="col-lg-4 text-center">
+              <img src="{{ asset('Imagenes/ICONOS/Logo cine footer.png') }}" alt="Logo footer" id="logoSecundario">
+              <p> Diseñado por <i>Gonzalez - Paradela -  Pastor</i></p>
+          </div>
+
+            <div class="col-lg-4">
+             <ul id="listafooter">
+              <li>
+                <a  href="#">Cartelera</a>
+              </li>
+              <li>
+                <a  href="#">Contacto</a>
+              </li>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Ayuda</a>
+              </li>
+              <li>
+                <a href="#">Promociones</a>
+              </li>
+            </ul>
+            </div>
+          </footer>
