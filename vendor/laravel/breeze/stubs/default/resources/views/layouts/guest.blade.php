7e800c0e844ee04aff64d448ab843cea24a3c265
Joaquin Paradela <joaquin.paradela28@gmail.com>
2023-08-02 12:29:47 -0300

    secure 5

--- vendor/laravel/breeze/stubs/default/resources/views/layouts/guest.blade.php
+++ vendor/laravel/breeze/stubs/default/resources/views/layouts/guest.blade.php
@@ -12,8 +12,12 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite([secure_asset('resources/css/app.css'), secure_asset('resources/js/app.js'), [secure_asset('build/assets/app-e7c8c463.js'), secure_asset('build/assets/app-385867e0.css')])
-</head>
+        @vite([
+        secure_asset('resources/css/app.css'), 
+        secure_asset('resources/js/app.js'), 
+        secure_asset('build/assets/app-e7c8c463.js'), 
+        secure_asset('build/assets/app-385867e0.css')
+       ])
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
