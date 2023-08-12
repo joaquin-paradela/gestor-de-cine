fc4db2e6bf8623e96040cb244bc68b1fbba1e805
Joaquin Paradela <joaquin.paradela28@gmail.com>
2023-08-12 17:35:34 -0300

    logo login

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img id="logo-principal" src="{{ secure_asset('imagenes/ICONOS/Logo cine png.png') }}" alt="Logo" style="width: 170px; height: 150px;">
+<img id="logo-principal" src="{{ secure_asset('Imagenes/ICONOS/Logo cine png.png') }}" alt="Logo" style="width: 170px; height: 150px;">
